9aa773a888165e027294fdb25436f2c470d2dd97
Girban Adhikari <adgirban1@gmail.com>
2025-02-01 15:06:53 +0545

    add sample video and read frames

---
+++ main.py
@@ -0,0 +1,29 @@
+from ultralytics import YOLO
+import cv2
+
+# load models
+coco_model = YOLO('yolo11n.pt')
+license_plate_detector = YOLO('best.pt')
+
+# load video
+cap = cv2.VideoCapture('sample.mp4')
+
+vehicles = [2, 3, 5, 7]
+
+# read frames
+frame_nmr = -1
+ret = True
+while ret:
+    frame_nmr += 1
+    ret, frame = cap.read()
+    if ret and frame_nmr < 10:
+        # detect vehicles
+        detections = coco_model(frame)[0]
+        detections_= []
+        for detection in detections.boxes.data.tolist():
+            x1, y1, x2, y2, score, class_id = detection
+            if int(class_id) in vehicles:
+                detections_.append([x1, y1, x2, y2, score])
+
+        # track vehicles
+        
